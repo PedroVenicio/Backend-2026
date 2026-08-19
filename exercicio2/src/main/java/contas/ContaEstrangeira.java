a4f1738a61b5cfa8c04e90c76b9e0888325ed7ad
Pedro Venicio <pedrovenicio456@gmail.com>
2026-08-19 18:01:56 -0300

    começo exercicio 2

---
+++ exercicio2/src/main/java/contas/ContaEstrangeira.java
@@ -0,0 +1,11 @@
+package contas;
+
+public class ContaEstrangeira extends Conta {
+
+    private static final double COTACAO_DOLAR = 5.40;
+
+    public ContaEstrangeira(String titular, String numero) {super(titular, numero);}
+
+
+
+}
